f384aca1d3e6b27dae8120fc02d3ce3f5269bea3
GitHub Action Example <action@github.com>
2020-01-09 23:34:08 +0000

    Added changes from zeryx/hello_world_algorithmia

---
+++ src/other_hello.py
@@ -0,0 +1,6 @@
+import Algorithmia
+
+# API calls will begin at the apply() method, with the request body passed as 'input'
+# For more details, see algorithmia.com/developers/algorithm-development/languages
+def apply(input):
+    return "hello from 'other_hello': https://github.com/zeryx/other_hello/edit/master/src/other_hello.py"
